e4a797bebed9385603139c2caa0607ae59741107
Sm <omni@qp>
2023-04-19 21:54:43 +0700

    fix to_dict in menu

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -8,5 +8,5 @@ from lexicons.lexicon_ru import MenuCommandsArgs
 async def set_main_menu(bot: Bot) -> None:
     main_menu_commands = [BotCommand(
         **kwargs
-    ) for kwargs in MenuCommandsArgs.asdict().values()]
+    ) for kwargs in MenuCommandsArgs.to_dict().values()]
     await bot.set_my_commands(main_menu_commands)
