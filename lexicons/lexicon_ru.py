6fa5aa24780a559867f9172b2b72d2f1edf42362
Sm <omni@qp>
2023-04-19 21:40:13 +0700

    add ru lexicon

---
+++ lexicons/lexicon_ru.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Answers:
+    start = '<b>Привет, читатель!</b>\n\nЭто бот, в котором '
+    'ты можешь прочитать книгу Рэя Брэдбери "Марсианские '
+    'хроники"\n\nЧтобы посмотреть список доступных '
+    'команд - набери /help'
+
+    help = '<b>Это бот-читалка</b>\n\nДоступные команды:\n\n/beginning - '
+    'перейти в начало книги\n/continue - продолжить '
+    'чтение\n/bookmarks - посмотреть список закладок\n/help - '
+    'справка по работе бота\n\nЧтобы сохранить закладку - '
+    'нажмите на кнопку с номером страницы\n\n<b>Приятного чтения!</b>'
+
+    no_bookmarks = 'У вас пока нет ни одной закладки.\n\nЧтобы '
+    'добавить страницу в закладки - во время чтения '
+    'книги нажмите на кнопку с номером этой '
+    'страницы\n\n/continue - продолжить чтение'
+
+    bookmarks = '<b>Это список ваших закладок:</b>'
+    edit_bookmarks = '<b>Редактировать закладки</b>'
+
+    cancel_text = '/continue - продолжить чтение'
+
+
+@dataclass
+class ButtonsNames:
+    edit_bookmarks = '❌ РЕДАКТИРОВАТЬ'
+    delete = '❌'
+    cancel = 'ОТМЕНИТЬ'
+    forward = '>>'
+    backward = '<<'
+
+
+@dataclass
+class MenuCommandsArgs:
+    beginning = {'command': '/beginning', 'description': 'В начало книги'}
+    continue_com = {'command': '/continue', 'description': 'Продолжить чтение'}
+    bookmarks = {'command': '/bookmarks', 'description': 'Мои закладки'}
+    help = {'command': '/help', 'description': 'Справка по работе бота'}
